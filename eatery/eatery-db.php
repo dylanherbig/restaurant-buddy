a2db04f4658c645a4f46aae0bae5534c93d390ad
Yusef Tarik Essawi <yte9fbr@portal05.cs.Virginia.EDU>
2023-12-02 17:29:55 -0500

    added form to add eatery, problems with current functionality

--- eatery/eatery-db.php
+++ eatery/eatery-db.php
@@ -30,4 +30,28 @@ function deleteFriend($name) {
     $statement->closeCursor();
 }
 
+function addEatery($name, $email, $description){
+    global $db;
+    $query = "insert into eatery (name, email, description)
+    VALUES (:name, :email, :description)";
+    $statement = $db->prepare($query); 
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':description', $description);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+function getEatery($name, $email){
+    global $db;
+    $query = "select * FROM eatery WHERE name=:name AND email=:email";
+    $statement = $db->prepare($query); 
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':email', $email);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    return $results;
+}
+
 ?>
