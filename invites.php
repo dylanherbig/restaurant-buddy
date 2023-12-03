bc5d9968ca77e6ff60fa2bf125487d678263076b
Dylan C Herbig <dch6auf@portal02.cs.Virginia.EDU>
2023-12-03 13:33:08 -0500

    fully integrated invitation handling into website

--- invites.php
+++ invites.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 require("connect-db.php");
 require("invites/invites-db.php");
 require("auth/user-db.php");
+include("header.html");
 
 // check if user is logged in, if not redirect to login.php
 $user = $_COOKIE["user"];
@@ -13,9 +14,25 @@ if (strlen($password) == 0 or strlen($user) == 0 or !checkUserPassword($user, $p
     header('Location: https://www.cs.virginia.edu/~dch6auf/project/auth/login.php');
 }
 
-// fetch all incoming invites
-$received_invites = fetchInviteeInvites($user);
-$sent_invites = fetchInviterInvites($user);
+// fetch all incoming and outgoing invites
+$received_invites = fetchReceivedInvites($user);
+$sent_invites = fetchSentInvites($user);
+
+// handle post requests
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+
+    if (!empty($_POST['deleteInvitation'])) {
+        deleteInvitation($_POST['delete_invitation_id']);
+    } else if (!empty($_POST['acceptInvitation'])) {
+        acceptInvitation($_POST['accept_invitation_id']);
+    } else if (!empty($_POST['denyInvitation'])) {
+        denyInvitation($_POST['deny_invitation_id']);
+    } else if (!empty($_POST['cancelInvitation'])) {
+        cancelInvitation($_POST['cancel_invitation_id']);
+    }
+    header("Refresh:0"); // refresh page for users to see changes
+}
 
 ?>
 
@@ -25,7 +42,7 @@ $sent_invites = fetchInviterInvites($user);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="../static/css/auth.css" rel="stylesheet" crossorigin="anonymous" />
 
@@ -39,7 +56,113 @@ $sent_invites = fetchInviterInvites($user);
 <body>
     <div id="header"></div>
 
-    <p>hello what is going on</p>
+    <div class="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
+        <div class="card bg-glass">
+            <div class="card-body px-4 py-5 px-md-5">
+                <h3>Received Invites</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Inviter</th>
+                            <th scope="col">Eatery</th>
+                            <th scope="col">From</th>
+                            <th scope="col">Until</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($received_invites as $invite) {
+                            $id = $invite['id']; // extract id 
 
+                            echo "<tr>";
+                            echo "<td>{$invite['full_name']}</td>";
+                            echo "<td>{$invite['name']}</td>";
+                            echo "<td>{$invite['date_time_from']}</td>";
+                            echo "<td>{$invite['date_time_to']}</td>";
+                            if ($invite["status"] == "pending") { 
+                                echo "<td>Pending</td>";
+                                echo "<td>
+                                        <form action='invites.php' method='post'>
+                                            <input type='submit' name='acceptInvitation' value='Accept' class='btn btn-secondary' />
+                                            <input type='hidden' name='accept_invitation_id' value='$id'/>
+                                        </form>
+                                        <form action='invites.php' method='post'>
+                                            <input type='submit' name='denyInvitation' value='Deny' class='btn btn-danger' />
+                                            <input type='hidden' name='deny_invitation_id' value='$id'/>
+                                        </form>
+                                    </td>";
+                            } else if ($invite["status"] == "accepted") {
+                                echo "<td>Accepted</td>";
+                                echo "<td>
+                                        <form action='invites.php' method='post'>
+                                            <input type='submit' name='cancelInvitation' value='Cancel' class='btn btn-danger' />
+                                            <input type='hidden' name='cancel_invitation_id' value='$id'/>
+                                        </form>
+                                    </td>";
+                            } else if ($invite["status"] == "denied") {
+                                echo "<td>Denied</td>";
+                                echo "<td>
+                                        &nbsp
+                                    </td>";
+                            } else if ($invite["status"] == "cancelled") {
+                                echo "<td>Cancelled</td>";
+                                echo "<td>
+                                        &nbsp
+                                    </td>";
+                            }
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+
+                <h3>Sent Invites</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Invitee</th>
+                            <th scope="col">Eatery</th>
+                            <th scope="col">From</th>
+                            <th scope="col">Until</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($sent_invites as $invite) {
+                            echo "<tr>";
+                            echo "<td>{$invite['full_name']}</td>";
+                            echo "<td>{$invite['name']}</td>";
+                            echo "<td>{$invite['date_time_from']}</td>";
+                            echo "<td>{$invite['date_time_to']}</td>";
+                            if ($invite["status"] == "pending") { 
+                                echo "<td>Pending</td>";
+                            } else if ($invite["status"] == "accepted") {
+                                echo "<td>Accepted</td>";
+                            } else if ($invite["status"] == "denied") {
+                                echo "<td>Denied</td>";
+                            } else {
+                                echo "<td>Cancelled</td>";
+                            } 
+                            
+                            $id = $invite['id'];
+                            echo "<td>
+                                    <form action='invites.php' method='post'>
+                                        <input type='submit' name='deleteInvitation' value='Delete' class='btn btn-danger' />
+                                        <input type='hidden' name='delete_invitation_id' value='$id'/>
+                                    </form>
+                                </td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- invites/invites-db.php
+++ invites/invites-db.php
@@ -1,6 +1,7 @@
 <?php
 
-function invite($invitee, $inviter, $eateryID, $date_time_from, $date_time_to) {
+function invite($invitee, $inviter, $eateryID, $date_time_from, $date_time_to)
+{
     global $db;
     $query = "insert into invites (invitee, inviter, eateryID, date_time_from, date_time_to, accepted, denied)
     VALUES (:invitee, :inviter, :eateryID, :date_time_from, :date_time_to, FALSE, FALSE)";
@@ -15,24 +16,82 @@ function invite($invitee, $inviter, $eateryID, $date_time_from, $date_time_to) {
     return $result;
 }
 
-function fetchInviteeInvites($invitee) {
+function fetchReceivedInvites($invitee)
+{
     global $db;
-    $query = 'select * from invites where invitee = :invitee';
-    $statement = $db->prepare($query); 
+    $query = 'SELECT 
+        invites.id,
+        user.full_name, 
+        eatery.name,
+        invites.date_time_from,
+        invites.date_time_to,
+        invites.status
+    FROM invites JOIN eatery JOIN user
+    ON invites.eateryID = eatery.ID AND invites.invitee = user.username
+    WHERE invites.invitee = :invitee';
+    $statement = $db->prepare($query);
     $statement->bindValue(':invitee', $invitee);
     $statement->execute();
     $result = $statement->fetchAll();
     return $result;
 }
 
-function fetchInviterInvites($inviter) {
+function fetchSentInvites($inviter)
+{
     global $db;
-    $query = 'select * from invites where inviter = :inviter';
-    $statement = $db->prepare($query); 
-    $statement->bindValue(':invitee', $inviter);
+    $query = 'SELECT 
+        invites.id,
+        user.full_name, 
+        eatery.name,
+        invites.date_time_from,
+        invites.date_time_to,
+        invites.status
+    FROM invites JOIN eatery JOIN user
+    ON invites.eateryID = eatery.ID AND invites.invitee = user.username
+    WHERE invites.inviter = :inviter';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':inviter', $inviter);
     $statement->execute();
     $result = $statement->fetchAll();
     return $result;
 }
+
+function deleteInvitation($invitationID) {
+    global $db;
+    $query = 'delete from invites where ID = :invitationID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':invitationID', $invitationID);
+    $statement->execute();
+    $statement->closeCursor();  
+}
+
+function acceptInvitation($invitationID) {
+    global $db;
+    $query = 'update invites set status = "accepted" where ID = :invitationID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':invitationID', $invitationID);
+    $statement->execute();
+    $statement->closeCursor();  
+}
+
+function denyInvitation($invitationID) {
+    global $db;
+    $query = 'update invites set status = "denied" where ID = :invitationID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':invitationID', $invitationID);
+    $statement->execute();
+    $statement->closeCursor();  
+}
+
+function cancelInvitation($invitationID) {
+    global $db;
+    $query = 'update invites set status = "cancelled" where ID = :invitationID';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':invitationID', $invitationID);
+    $statement->execute();
+    $statement->closeCursor();  
+}
+
+
 
 ?>
